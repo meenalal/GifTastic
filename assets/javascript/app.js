f14a885da81810a79bd68f89ce44a1fe39a195a5
Meenal <meenalalagappan89@gmail.com>
2018-11-03 23:44:22 -0700

    final code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,148 +1,118 @@
-// Wait for page to load
-$(document).ready(function()
-{
-	// Initial array of topics
-	var topics = ['The Simpsons', 'Bugs Bunny', 'Mickey Mouse', 'Donald Duck', 'Tom and Jerry', 'Babies'];
+$(document).ready(function(){
+	// Initial array of animes
+	var animes = ["Sailor Moon Crystal", "Fairy Tail", "Yowamushi", "kuroko no basket", "Chihayafuru", "Yona of the Dawn", "Blue Exorcist"];
 
-	// Generic function for displaying topic data 
-	function renderButtons()
-	{
-		// Deletes the topics prior to adding new topics (this is necessary otherwise you will have repeat buttons)
-		$('#topicsView').empty();
+	// displayAnimeShow function re-renders the HTML to display the appropriate content
+	function displayAnimeShow() {
 
-		// Loops through the array of topics
-		for (var i = 0; i < topics.length; i++)
-		{
-			// Dynamically generate buttons for each topic in the array
+	  var anime = $(this).attr("data-name");
+	  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + anime + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-			// jQUery syntax here... 
-			var a = $('<button type="button">') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-			a.addClass('topicButton'); // Added a class 
-			a.addClass('btn btn-primary'); // Added a class 
-			a.attr('data-name', topics[i]); // Added a data-attribute
-			a.text(topics[i]); // Provided the initial button text
-			$('#topicsView').append(a); // Added the button to the HTML
+	  // Creates AJAX call for the specific movie button being clicked
+	  $.ajax({
+		url: queryURL,
+		method: "GET"
+	  }).done(function(response){
+		$("#animeview").empty();
+
+		var results = response.data;
+
+		// Retrieves the Rating Data
+		console.log(response);
+
+		// Loops the anime for limit 10
+		for(var i = 0; i < results.length; i++) {
+
+		  // Creates a div to hold the anime
+		  var animeDiv = $("<div>");
+
+		  // Make the class for style.css
+		  animeDiv.addClass("animepictures");
+
+		  // Creates an element to have the rating displayed
+		  var rating = results[i].rating;
+		  var p = $("<h2>").text("Rating: " + rating);
+
+		  // The Images can still or animate to call the class "animeImage" for click.
+		  var animeImage = $("<img>");
+		  animeImage.attr("src", results[i].images.fixed_height_still.url);
+		  animeImage.attr("data-still", results[i].images.fixed_height_still.url);
+		  animeImage.attr("data-animate", results[i].images.fixed_height.url);
+		  animeImage.attr("data-state", "still");
+		  animeImage.addClass('animeImage');
+
+		  // Displays the rating
+		  animeDiv.prepend(p);
+
+		  // Displays the anime Image
+		  animeDiv.prepend(animeImage);
+		  $("#animeview").prepend(animeDiv);
 		}
-	}
 
-		// ========================================================
+		//if the variable state is equal to 'still',
+		// then update the src attribute of this image to it's data-animate value,
+		// and update the data-state attribute to 'animate'.
+		// If state does not equal 'still', then update the src attribute of this
+		// image to it's data-animate value and update the data-state attribute to 'still'
+		$(".animeImage").on("click", function() {
+		  var state = $(this).attr("data-state");
+		  console.log(state);
 
-		// This function handles events where one button is clicked
-		$('#addTopic').on('click', function(){
-
-			console.log('button clicked');
-
-
-			// This line of code will grab the input from the textbox
-			var topic = $('#topicInput').val().trim();
-
-			console.log(topic);
-			if (topic != "")
-			{
-				// The topic from the textbox is then added to our array
-				topics.push(topic);
-				// Our array then runs which handles the processing of our topic array
-				renderButtons();
-			}
-
-			else
-			{
-				$('#topicInput').attr("placeholder", "Please enter a topic to search.")
-				renderButtons();
-			}
-
-			
-
-
-			// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
-			return false;
+		  if (state === "still") {
+			$(this).attr("src", $(this).attr("data-animate"));
+			$(this).attr("data-state", "animate");
+		  } else {
+			$(this).attr("src", $(this).attr("data-still"));
+			$(this).attr("data-state", "still");
+		  }
 		});
-
-
-	//Function for displaying gifs and still images
-	function displaytopicGif()
-	{
-
-		$('#gifView').empty();
-		var topic = $(this).attr('data-name');
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10&offset=0";
-
-
-		$.ajax({url: queryURL, method: 'GET'})
-		.done(function(response) 
-		{
-
-			// Creates a generic div to hold the topic
-			var topicDiv = $('<div class="topicImage">');
-			console.log(response);
-			for (i=0; i < response.data.length; i++) 
-			{
-				var stillImage = response.data[i].images.fixed_height_still.url;
-				console.log(stillImage);
-
-				var playImage = response.data[i].images.fixed_height.url;
-				console.log("Moving"+ playImage);
-
-				var rating = response.data[i].rating;
-				console.log(rating);
-
-				// Creates an element to have the rating displayed
-				var pOne = $('<p>').text( "Rating: " + rating.toUpperCase());
-				topicDiv.append(pOne);
-
-				var image = $("<img>").attr("src", stillImage); //Passes still image link to the image src
-				image.attr("playsrc", playImage); //Creates playsrc attr and passes moving gif link to the image playsrc
-				image.attr("stopsrc", stillImage); //Creates stopsrc attr and passes still image link to the image stopsrc
-				
-				topicDiv.append(image);
-
-				// Puts the entire topic above the previous celebrities.
-				$('#gifView').append(topicDiv);
-
-				image.addClass('playClickedGif'); // Added a class to image tag
-
-
-			}	
-		});
+	  });        
 	}
 
-	function swapGif()
-	{
-		//Stop Image 
-		var playImage = $(this).attr('playsrc');
+	// Function for displaying anime data
+	function renderButtons() {
 
-		console.log(playImage);
+	  // Deletes the movies prior to adding new movies
+	  // (this is necessary otherwise you will have repeat buttons)
+	  $("#animebuttons").empty();
 
+	  for(var i = 0; i < animes.length; i++) {
 
-		//Stop Image 
-		var stopImage = $(this).attr('stopsrc');
+		// Then dynamicaly generates buttons for each movie in the array
+		// This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+		var animeAdd = $("<button>");
 
-		console.log(stopImage);
+		// Adds a class of anime to our button
+		animeAdd.addClass("anime");
 
-		//Swap image condition
-		if ($(this).attr('playsrc') == $(this).attr('src'))
-		{
-			//This changes the image src
-			$(this).attr('src', stopImage);
-		}
+		// Added a data-attribute
+		animeAdd.attr("data-name", animes[i]);
 
-		else
-		{
-			$(this).attr('src', playImage);
-		}
+		// Provided the initial button text
+		animeAdd.text(animes[i]);
+
+		// Added the button to the buttons-view div
+		$("#animebuttons").append(animeAdd);
+	  }
 	}
 
-		
+	// This function handles events where the add anime button is clicked
+	$("#add-anime").on("click", function(event){
+	  event.preventDefault();
 
+	  // This line of code will grab the input from the textbox
+	  var anime = $("#anime-input").val().trim();
 
-	// ========================================================
+	  // The anime from the textbox is then added to our array
+	  animes.push(anime);
 
-	// This calls the renderButtons() function
+	  // Calling renderButtons which handles the processing of our anime array
+	  renderButtons();
+	});
+
+	// Adding click event listeners to all elements with a class of "anime"
+	$(document).on("click", ".anime", displayAnimeShow);
+
+	// Calling the renderButtons function to display the intial buttons
 	renderButtons();
-
-	// Generic function for displaying the Gif
-	$(document).on('click', '.topicButton', displaytopicGif);
-	$(document).on('click', '.playClickedGif', swapGif);
-
-
 });
